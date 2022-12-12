08d859c95077587100a6df59d11a079388e19001
Xinzhe Chai <xinzhe_chai@brown.edu>
2022-12-12 16:33:08 -0500

    wrote some code

--- Final_Code.ino
+++ Final_Code.ino
@@ -5,7 +5,7 @@
 #define DEMO_MODE
 #define VERBOSE 1 //Higher number is more verbose. Ranges from [0-3], with 0 being only errors shown, and 3 being insane amounts of output.
 
- #define RUN_UNIT_TESTS // uncomment to run unit tests
+#define RUN_UNIT_TESTS // uncomment to run unit tests
 
 #ifdef RUN_UNIT_TESTS
 #define MOCK_FUNCTIONS // uncomment to make update_fsm call mock functions
@@ -46,7 +46,7 @@
 #define MS_UNTIL_DOUBLEPRESS_REGISTERED 1500
 #define PHOTORESISTOR_SAMPLE_INTERVAL_MS 100 //ms between light sensor samples
 #define MS_UNTIL_BUTTON_PRESS_REGISTERED 60
-#define LOOPS_UNTIL_DOUBLEPRESS_REGISTERED (((MS_UNTIL_DOUBLEPRESS_REGISTERED - LOOP_INTERVAL) / LOOP_INTERVAL) + 1)
+#define LOOPS_UNTIL_DOUBLEPRESS_REGISTERED (((MS_UNTIL_DOUBLEPRESS_REGISTERED - LOOP_INTERVAL) / LOOP_INTERVAL) + 1) // 12
 #define LOOPS_UNTIL_BUTTON_PRESS_REGISTERED (((MS_UNTIL_BUTTON_PRESS_REGISTERED - LOOP_INTERVAL) / LOOP_INTERVAL) + 1)
 #define LOOPS_PER_PHOTORESISTOR_SAMPLE (PHOTORESISTOR_SAMPLE_INTERVAL_MS / LOOP_INTERVAL)
 #define SAMPLES_TO_KEEP ((1000 * MOVING_AVERAGE_INTERVAL_SECONDS) / PHOTORESISTOR_SAMPLE_INTERVAL_MS)
@@ -611,7 +611,7 @@ void Button_ISR() {
   mils_at_last_physical_interaction = millis();
 }
 
-FSM_State test_state;
+// FSM_State test_state;
 Motor_Direction test_direction;
 bool test_up_button;
 bool test_down_button;
@@ -619,27 +619,80 @@ uint32_t test_photo_resister_avg;
 uint32_t test_up_loop_count;
 uint32_t test_down_loop_count;
 
-void resetInput()
+void initializeInput()
 {
-	test_direction = Motor_Direction::Motor_OFF;
-	test_state = FSM_State::S_INIT;
-	test_up_button = false;
-	test_down_button = false;
-	uint32_t test_photo_resister_avg = 300;
-	uint32_t test_up_loop_count = 0;
-	uint32_t test_down_loop_count = 0;
+  test_direction = Motor_Direction::Motor_OFF;
+  curr_system_state = FSM_State::S_INIT;
+  test_up_button = false;
+  test_down_button = false;
+  test_photo_resister_avg = 500;
+  test_up_loop_count = LOOPS_UNTIL_DOUBLEPRESS_REGISTERED;
+  test_down_loop_count = LOOPS_UNTIL_DOUBLEPRESS_REGISTERED;
 }
 
 void runAllTests()
 {
 	bool testPassFlag = true;
-	resetInput();
-	// test setting up system
+	initializeInput();
+
+	// state 1-2
 	test_up_button = true;
-	test_down_button = true;
-	fsmUpdate(test_up_button, test_down_button, false, test_photo_resister_avg, test_up_loop_count, test_down_loop_count);
-	if (test_state != S_WAIT) {
+	test_down_button = false;
+	fsmUpdate(test_up_button, test_down_button, false, test_photo_resister_avg, test_up_loop_count + 10, test_down_loop_count + 10);
+	if (curr_system_state != S_SETUP_MAX) {
+    log(0, "TEST: test 1-2 failed, current FSM state: %d\n", curr_system_state);
 		testPassFlag = false;
+	}
+
+  // state 2-3
+	// both buttons released, loop count needs to be greater than threshold
+  test_up_button = false;
+  test_down_button = false;
+  fsmUpdate(test_up_button, test_down_button, false, test_photo_resister_avg, test_up_loop_count + 10, test_down_loop_count + 10);
+  if (curr_system_state != S_SETUP_MIN) {
+    log(0, "TEST: test 2-3 failed, current FSM state: %d\n", curr_system_state);
+    testPassFlag = false;
+  }
+
+	// state 3-4
+	// making sure the blind is not below our lowest bottom
+	micros_motor_lowered = MOTOR_MICROS_MARGIN + 1000;
+  test_up_button = false;
+  test_down_button = false;
+  fsmUpdate(test_up_button, test_down_button, false, test_photo_resister_avg, test_up_loop_count + 10, test_down_loop_count + 10);
+  if (curr_system_state != S_WAIT) {
+    log(0, "TEST: test 3-4 failed, current FSM state: %d\n", curr_system_state);
+    testPassFlag = false;
+  }
+
+	// state 4-5
+	// blinder should go up when DOWN button is pressed
+  test_up_button = true;
+  test_down_button = false;
+  fsmUpdate(test_up_button, test_down_button, false, test_photo_resister_avg, test_up_loop_count + 10, test_down_loop_count - 10);
+  if (curr_system_state != S_BUTTON_UP) {
+    log(0, "TEST: test 4-5 failed, current FSM state: %d\n", curr_system_state);
+    testPassFlag = false;
+  }
+
+	// state 5-4
+	// press the up button again, or the position is MAX; blinder should go to S_WAIT
+	test_up_button = true;
+  test_down_button = true;
+  // make double press to true
+	fsmUpdate(test_up_button, test_down_button, false, test_photo_resister_avg, test_up_loop_count + 10, test_down_loop_count + 10);
+	test_up_button = false;
+  test_down_button = false;
+	fsmUpdate(test_up_button, test_down_button, false, test_photo_resister_avg, test_up_loop_count - 10, test_down_loop_count - 10);
+  if (curr_system_state != S_WAIT) {
+    log(0, "TEST: test 5-4 failed, current FSM state: %d\n", curr_system_state);
+    testPassFlag = false;
+  }
+
+	if (testPassFlag) {
+			log(0, "TEST: all tests passed\n");
+	} else {
+      log(0, "TEST: test suite failed\n");
 	}
 	
 }
